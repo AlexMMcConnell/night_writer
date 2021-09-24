d8a6c1f30697386e34533adebdb32e9af5a994fc
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-23 19:23:45 -0600

    Feat - Created Writer class

---
+++ lib/writer.rb
@@ -0,0 +1,19 @@
+class Writer
+  attr_reader :text_file,
+              :translate_file
+
+  def initialize(files)
+    @text_file = files[0]
+    @translate_file = files[1]
+  end
+
+  def split_text
+    split_text = File.readlines(text_file, chomp: true).flat_map do |letter|
+      letter.split('')
+    end
+  end
+
+  def creation_message
+    p "Created '#{translate_file}' containing #{split_text.length} characters."
+  end
+end
