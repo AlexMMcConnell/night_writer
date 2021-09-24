fdc7f2657ce479b7c20688a263fe9c2759750510
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-23 19:34:09 -0600

    Test - Adjust testing files for file location

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -1,12 +1,12 @@
 require 'SimpleCov'
 SimpleCov.start
 
-require './writer'
+require './lib/writer'
 
 describe Writer do
   before(:each) do
-    text_files = ['message1.txt', 'translate1.txt']
-    @writer = Writer.new(text_files)
+    @text_files = ['./lib/message1.txt', 'translate1.txt']
+    @writer = Writer.new(@text_files)
   end
 
   it 'is an instance of Writer' do
@@ -14,7 +14,7 @@ describe Writer do
   end
 
   it 'has readable attributes' do
-    expect(@writer.text_file).to eq('message1.txt')
+    expect(@writer.text_file).to eq('./lib/message1.txt')
     expect(@writer.translate_file).to eq('translate1.txt')
   end
 
