c7ba9ddacfcd74383815cde91a58434cc94a3e94
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-24 18:55:07 -0600

    Feat - Add translator method, modified creation_message

--- lib/writer.rb
+++ lib/writer.rb
@@ -1,10 +1,14 @@
+require_relative 'hash_creator'
+
 class Writer
   attr_reader :text_file,
-              :translate_file
+              :translate_file,
+              :creator
 
   def initialize(files)
     @text_file = files[0]
     @translate_file = files[1]
+    @creator = HashCreator.new
   end
 
   def split_text
@@ -14,6 +18,13 @@ class Writer
   end
 
   def creation_message
-    puts "Created '#{translate_file}' containing #{split_text.length} characters."
+    p "Created '#{translate_file}' containing #{split_text.length} characters."
+  end
+
+  def translator
+    text_to_braille_hash = creator.text_to_braille
+    split_text.map do |letter|
+      text_to_braille_hash[letter]
+    end
   end
 end
