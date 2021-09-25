51c7069dbcdcd23b854ee2fce72902648ef38206
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-25 16:41:29 -0600

    Test - Finalized test suite for reader class

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -5,7 +5,7 @@ require './lib/reader'
 
 describe Reader do
   before(:each) do
-    @text_files = ['translated1.txt', 'translated2.txt']
+    @text_files = ['braille1.txt', 'translated2.txt']
     @reader = Reader.new(@text_files)
   end
 
@@ -14,25 +14,25 @@ describe Reader do
   end
 
   it 'has readable attributes' do
-    expect(@reader.text_file).to eq('translated1.txt')
-    expect(@reader.translate_file).to eq('translatd2.txt')
+    expect(@reader.text_file).to eq('braille1.txt')
+    expect(@reader.translate_file).to eq('translated2.txt')
   end
 
-  describe 'split_text' do
+  describe 'split_braille' do
     it 'splits text into an array' do
-      allow(@reader).to receive(:text_file).and_return('translated1.txt')
+      allow(@reader).to receive(:text_file).and_return('braille1.txt')
 
-      expect(@reader.split_text.length).to eq(46)
-      expect(@reader.split_text).to be_a(Array)
+      expect(@reader.split_braille.length).to eq(46)
+      expect(@reader.split_braille).to be_a(Array)
     end
   end
 
   describe 'creation_message' do
     it 'sends a message confirming creation of the new file' do
       allow(@reader).to receive(:translate_file).and_return('translated2.txt')
-      allow(@reader).to receive(:split_text).and_return([1, 2, 3, 4])
+      allow(@reader).to receive(:split_braille).and_return([1, 2, 3, 4])
 
-      expected = "Created 'translate1.txt' containing 4 characters."
+      expected = "Created 'translated2.txt' containing 4 characters."
       expect(@reader.creation_message).to eq(expected)
     end
   end
@@ -40,7 +40,13 @@ describe Reader do
   describe 'translator' do
     it 'translates from braille to english' do
       expect(@reader.translator).to be_a(Array)
-      expect(@reader.translator[0]).to eq(['h'])
+      expect(@reader.translator[0]).to eq('h')
+    end
+  end
+
+  describe 'reader' do
+    it 'reads all braille into a new text file' do
+      expect(@reader.read).to eq([])
     end
   end
 end
